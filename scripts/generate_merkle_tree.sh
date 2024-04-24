d529920ab0daa6d770c63476bb41718b5967ebf0
shuse2 <shus.toda@gmail.com>
2024-04-24 15:08:25 +0200

    fix the order of the network variable

--- scripts/generate_merkle_tree.sh
+++ scripts/generate_merkle_tree.sh
@@ -2,17 +2,18 @@
 
 S3_BUCKET_NAME="lisk-migration-assets"
 FOLDER_NAME="token-claim"
-NETWORK="testnet"
 OUTPUT_DIR="./data/$FOLDER_NAME"
+
+NETWORK="testnet"
+if [ -n "$1" ]; then
+    NETWORK="$1"
+fi
+
 SNAPSHOT_URL="https://snapshots.lisk.com/$NETWORK/blockchain.db.tar.gz"
 EXCLUDE_ADDRESSES_PATH="./data/$NETWORK/exclude_addresses.txt"
 EXCLUDE_AIRDROP_ADDRESSES_PATH="./data/$NETWORK/excluded_airdrop_addresses.txt"
 OUTPUT_DIR_CLAIM="$OUTPUT_DIR/claim"
 OUTPUT_DIR_AIRDROP="$OUTPUT_DIR/airdrop"
-
-if [ -n "$1" ]; then
-    NETWORK="$1"
-fi
 
 if [ "$NETWORK" = "mainnet" ]; then
     TOKEN_ID="0000000000000000"
