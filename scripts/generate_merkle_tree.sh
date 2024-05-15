cfb172af5cdf92bcfdac58eb9f251af39d8bd4db
Franco NG <franco.ng@lightcurve.io>
2024-05-15 12:51:57 +0200

    Added placeholder excluded files and updated Dockerfile
    ---------
    
    Co-authored-by: shuse2 <shuse2@users.noreply.github.com>
    Co-authored-by: shuse2 <shus.toda@gmail.com>

--- scripts/generate_merkle_tree.sh
+++ scripts/generate_merkle_tree.sh
@@ -10,7 +10,8 @@ if [ -n "$1" ]; then
 fi
 
 SNAPSHOT_URL="https://snapshots.lisk.com/$NETWORK/blockchain.db.tar.gz"
-EXCLUDE_ADDRESSES_PATH="./data/exclude_addresses.txt"
+EXCLUDE_ADDRESSES_PATH="./data/$NETWORK/exclude_addresses.txt"
+OUTPUT_DIR_CLAIM="$OUTPUT_DIR/claim"
 
 if [ "$NETWORK" = "mainnet" ]; then
     TOKEN_ID="0000000000000000"
@@ -38,7 +39,13 @@ else
     echo "Output folder already exists"
 fi
 
-./packages/tree-builder/bin/run.js generate-merkle-tree --db-path=./tmp --output-path=$OUTPUT_DIR --token-id=$TOKEN_ID --excluded-addresses-path $EXCLUDE_ADDRESSES_PATH || exit 1
+if [ ! -d "$OUTPUT_DIR_CLAIM" ]; then
+    mkdir -p $OUTPUT_DIR_CLAIM
+else
+    echo "Claim output folder already exists"
+fi
+
+./packages/tree-builder/bin/run.js generate-merkle-tree --db-path=./tmp --output-path=$OUTPUT_DIR_CLAIM --token-id=$TOKEN_ID --excluded-addresses-path $EXCLUDE_ADDRESSES_PATH || exit 1
 
 # Upload files in $OUTPUT_DIR to S3 with folder token-claim
 aws s3 cp $OUTPUT_DIR s3://$S3_BUCKET_NAME/$FOLDER_NAME/$NETWORK --recursive
