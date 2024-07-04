a5c743559df9575bfc442b466745dc96ef641c52
Franco NG <franco.ng@lightcurve.io>
2024-07-04 18:51:56 +0200

    Update Testnet API endpoint

--- packages/claim-cli/src/utils/network-params.ts
+++ packages/claim-cli/src/utils/network-params.ts
@@ -15,7 +15,7 @@ export const Mainnet = {
 } as NetworkParams;
 
 export const Testnet = {
-	api: 'https://token-claim-api.lisk.com/rpc',
+	api: 'https://sepolia-token-claim-api.lisk.com/rpc',
 	rpc: 'https://rpc.sepolia-api.lisk.com',
 	l2Claim: '0x3D4190b08E3E30183f5AdE3A116f2534Ee3a4f94',
 	maxFeePerGas: BigInt(1002060),
